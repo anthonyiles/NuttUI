a50c14d03cb439e5ecd155a3a37f1bbb71cb2069
Anthony Iles <anthonyiles91@gmail.com>
2026-02-01 12:40:29 +0000

    minor improvements

--- modules/Databar/Slots/MythicKey.lua
+++ modules/Databar/Slots/MythicKey.lua
@@ -2,34 +2,38 @@ local _, NuttUI = ...
 
 NuttUI.Databar:RegisterSlot({
     name = "Mythic+ Key",
-    events = {"BAG_UPDATE"},
+    events = { "BAG_UPDATE" },
     interval = 10,
     Update = function(self, label)
         local mapID = C_MythicPlus.GetOwnedKeystoneChallengeMapID()
         local level = C_MythicPlus.GetOwnedKeystoneLevel()
-        
-        if not mapID or not level then return string.format("|cffffffff%s:|r None", label or "Key") end
-        
+
+        if not mapID or not level then
+            return string.format("|cffffffff%s:|r %sNone|r", label or "Key",
+                NuttUI:GetDatabarColor("|cffffffff"))
+        end
+
         local name = C_ChallengeMode.GetMapUIInfo(mapID)
-        return string.format("|cffffffff%s:|r |cffa335ee%s (%d)|r", label or "Key", name, level)
+        local color = NuttUI:GetDatabarColor("|cffa335ee")
+        return string.format("|cffffffff%s:|r %s%s (%d)|r", label or "Key", color, name, level)
     end,
     OnEnter = function(self)
         local mapID = C_MythicPlus.GetOwnedKeystoneChallengeMapID()
         local level = C_MythicPlus.GetOwnedKeystoneLevel()
-        
+
         GameTooltip:AddLine("Mythic+ Keystone")
         if mapID and level then
-             local name = C_ChallengeMode.GetMapUIInfo(mapID)
-             GameTooltip:AddDoubleLine("Dungeon:", name, 1, 1, 1, 1, 1, 1)
-             GameTooltip:AddDoubleLine("Level:", level, 1, 1, 1, 1, 1, 1)
+            local name = C_ChallengeMode.GetMapUIInfo(mapID)
+            GameTooltip:AddDoubleLine("Dungeon:", name, 1, 1, 1, 1, 1, 1)
+            GameTooltip:AddDoubleLine("Level:", level, 1, 1, 1, 1, 1, 1)
         else
-             GameTooltip:AddLine("No Keystone found in bags.", 0.6, 0.6, 0.6)
+            GameTooltip:AddLine("No Keystone found in bags.", 0.6, 0.6, 0.6)
         end
-        
+
         GameTooltip:AddLine(" ")
-        GameTooltip:AddLine("<Left-Click> to open Dungeon Journal", 0.5, 0.5, 0.5)
+        GameTooltip:AddLine("<Left-Click> to open Dungeons & Raids", 0.5, 0.5, 0.5)
     end,
     OnClick = function(self, button)
-        PVEFrame_ToggleFrame("PVEFrame", DungeonJournal)
+        ToggleFrame(PVEFrame)
     end
 })
